64a4ad2588fc07e9fcf897a93b6e518653434b2f
nergdvnlt <tylerlundgren@yahoo.com>
2018-02-27 10:54:01 -0700

    Figuring out how to return error for line in middle

--- lib/company.rb
+++ lib/company.rb
@@ -10,7 +10,7 @@ class Company
     @timesheets = []
   end
 
-  def check_success(filepath)
+  def check_success_employee(filepath)
     CSV.foreach(filepath) do |row|
       return 'Error: bad file' if row.length != 5
       return 'success' if row.length == 5
@@ -18,26 +18,37 @@ class Company
   end
 
   def load_employees(filepath)
-    if check_success(filepath) == 'success'
+    if check_success_employee(filepath) == 'success'
       CSV.foreach(filepath) do |row|
-        data = { id: row[0], name: row[1], start_date: row[2], end_date: row[3] }
-        @employees << data
+        @employees << { id: row[0],
+                        name: row[1],
+                        role: row[2],
+                        start_date: row[3],
+                        end_date: row[4] }
       end
     else
-      puts check_success(filepath)
-      check_success(filepath)
+      check_success_employee(filepath)
     end
   end
 
+  def check_success_project(filepath)
+    CSV.foreach(filepath) do |row|
+      return result = 'success' if row.length == 4
+      return result =  'Error: bad file' if row.length != 4
+    end
+    result
+  end
+
   def load_projects(filepath)
-    if check_success(filepath) == 'success'
+    if check_success_project(filepath) == 'success'
       CSV.foreach(filepath) do |row|
-        data = { project_id: row[0], name: row[1], start_date: row[2], end_date: row[3] }
-        @employees << data
+        @projects << { project_id: row[0],
+                       name: row[1],
+                       start_date: row[2],
+                       end_date: row[3] }
       end
     else
-      puts check_success(filepath)
-      check_success(filepath)
+      check_success_project(filepath)
     end
   end
 end
